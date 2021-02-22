9c0e5cd529081f4b032141990a52e80e3d639b15
huangyang <764735411@qq.com>
2021-02-22 12:27:44 +0800

    增加路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,32 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
+const Home = () => import('views/home/Home.vue')
+const Classify = () => import('views/classify/Classify.vue')
+const Cat = () => import('views/cat/ShopCat.vue')
+const Profile = () => import('../views/profile/Profile.vue')
 
+const routes = [
+  {
+    path:'/',
+    redirect:'/home'
+  },
+  {
+    path:'/home',
+    component:Home
+  },
+  {
+    path:'/profile',
+    component:Profile
+  },
+  {
+    path:'/classify',
+    component:Classify
+  },
+  {
+    path:'/cat',
+    component:Cat
+  },
 ]
 
 const router = new VueRouter({
